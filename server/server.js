306b8c22f2e01c9407824e4d3fcbb1d1acbf4a34
Giridhar <giridharv38@gmail.com>
2017-06-22 15:43:16 -0700

    Update for server.js code

--- server/server.js
+++ server/server.js
@@ -23,37 +23,3 @@ app.post('/todos',function (req,res) {
 app.listen(3000,function () {
     console.log("Server up on 3000")
 })
-
-
-// var newTodo = new Todo({
-//     text:'Be in downtown tommorow at 8am',
-//     completed:true,
-//     completedAt:1843
-// })
-//
-// newTodo.save().then(function (doc) {
-//     console.log(doc)
-// },function (err) {
-//     console.log(err)
-// })
-//
-// var todo = new Todo({
-//     text:'Just do it'
-// })
-//
-// todo.save().then(function (doc) {
-//     console.log(doc)
-// },function (err) {
-//     console.log(err)
-// })
-
-
-
-// var user = new User({
-//     email:'giridharv9@gmail.com'
-// })
-// user.save().then(function (doc) {
-//     console.log(doc)
-// },function (err) {
-//     console.log(err)
-// })
