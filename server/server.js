b9fc06d8dba2520ab6edaae2a3d8762014196dda
Giridhar <giridharv38@gmail.com>
2017-06-23 00:08:47 -0700

    updated server code

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ var Todo =require('./models/todo').Todo
 var Users = require('./models/users')
 var express = require('express')
 var bodyparser = require('body-parser')
+var ObjectId = require('mongodb').ObjectID
+
 
 var app = express()
 app.use(bodyparser.json())
@@ -19,6 +21,32 @@ app.post('/todos',function (req,res) {
     },function (err) {
         res.status(400).send(err)
     })
+})
+
+app.get('/todos',function (req,res) {
+    Todo.find().then(function (todos) {
+       res.send(todos)
+        console.log({todos})
+    },function (e) {
+        console.log(e)
+    })
+})
+
+app.get('/todos/:id',function (req,res) {
+    var id = req.params.id;
+    if(!ObjectId.isValid(id))
+    {
+
+    }
+    else
+    {
+        Todo.findOne({_id:id}).then(function (todo) {
+            res.send(todo)
+            console.log(todo)
+        },function (err) {
+            res.sendStatus(404).send(err)
+        })
+    }
 })
 app.listen(3000,function () {
     console.log("Server up on 3000")
